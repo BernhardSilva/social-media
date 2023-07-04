8d036a5abbe71326c77c5b5d1aefdf8f6c2292b5
bernhard.silva@gmail.com <bernhard.silva@gmail.com>
2023-07-03 21:57:31 -0400

    TODO FLAG

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,6 @@
 import express from 'express';
 import { getUser, getUserFriends, addRemoveFriend } from '../controllers/users.js';
 import { verifyToken } from '../middleware/auth.js';
+
+
+//TODO FLAG
