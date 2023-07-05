ebb9e1b1b334643a254bf9b6f389cce0db4bcb84
bernhard.silva@gmail.com <bernhard.silva@gmail.com>
2023-07-05 12:43:31 -0400

    updt users

--- routes/users.js
+++ routes/users.js
@@ -2,5 +2,7 @@ import express from 'express';
 import { getUser, getUserFriends, addRemoveFriend } from '../controllers/users.js';
 import { verifyToken } from '../middleware/auth.js';
 
+const router = express.Router()
 
-//TODO FLAG
+
+//TODO FLAG https://youtu.be/K8YELRmUb5o?t=3682
